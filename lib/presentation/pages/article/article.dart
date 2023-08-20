88546170b1bcdbbc0a2923abde0f3ecca37d1154
ezizull <mfiqor@gmail.com>
2023-08-20 13:46:05 +0700

    add: module3 v1

--- lib/presentation/pages/article/article.dart
+++ lib/presentation/pages/article/article.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:module_app/presentation/controllers/controllers.dart';
-import 'widget/article.widget.dart';
+import 'widget/card.widget.dart';
 
 class ArticlePage extends StatelessWidget {
   final dioController = Get.find<DioController>();
@@ -13,31 +13,33 @@ class ArticlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Obx(() {
-          if (getConnectController.isLoading.value) {
-            // Display a progress indicator while loading
-            return Center(
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.secondary),
-              ),
-            );
-          } else {
-            // Display the list of articles
-            return Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: getConnectController.articles.length,
-                itemBuilder: (context, index) {
-                  var article = getConnectController.articles[index];
-                  return CardArticle(article: article);
-                },
-              ),
-            );
-          }
-        }),
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          Obx(() {
+            if (getConnectController.isLoading.value) {
+              // Display a progress indicator while loading
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.secondary),
+                ),
+              );
+            } else {
+              // Display the list of articles
+              return Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: getConnectController.articles.length,
+                  itemBuilder: (context, index) {
+                    var article = getConnectController.articles[index];
+                    return CardArticle(article: article);
+                  },
+                ),
+              );
+            }
+          }),
+        ],
+      ),
     );
   }
 }
